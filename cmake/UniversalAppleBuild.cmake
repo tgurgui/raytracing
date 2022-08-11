8374517f46621a32fe3ac50d5e528a17f745102d
Toni Gurgui <toni.gurgui@gmail.com>
2022-08-11 17:03:31 +0200

    FIX: Do not build for arm64 arch in Apple

--- cmake/UniversalAppleBuild.cmake
+++ cmake/UniversalAppleBuild.cmake
@@ -1,5 +1,5 @@
 # Generate universal executable for Apple hardware
 # This file needs to be included before calling `project`.
 if (APPLE)
-  set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64" CACHE STRING "")
+  set(CMAKE_OSX_ARCHITECTURES "x86_64" CACHE STRING "")
 endif ()
