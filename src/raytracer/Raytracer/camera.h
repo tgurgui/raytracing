748e68850d77ee27f59a0050d593fba1a463b090
Toni Gurguí <toni.gurgui@galgomedical.com>
2023-11-18 14:58:29 +0100

    ENH: Remove "time" from Ray and Camera

--- src/raytracer/Raytracer/camera.h
+++ src/raytracer/Raytracer/camera.h
@@ -25,9 +25,7 @@ class camera {
             double vfov, // vertical field-of-view in degrees
             double aspect_ratio,
             double aperture,
-            double focus_dist,
-            double _time0 = 0,
-            double _time1 = 0
+            double focus_dist
         ) {
             auto theta = degrees_to_radians(vfov);
             auto h = tan(theta/2);
@@ -44,8 +42,6 @@ class camera {
             lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
 
             lens_radius = aperture / 2;
-            time0 = _time0;
-            time1 = _time1;
         }
 
         ray get_ray(double s, double t) const {
@@ -53,8 +49,7 @@ class camera {
             vec3 offset = u * rd.x() + v * rd.y();
             return ray(
                 origin + offset,
-                lower_left_corner + s*horizontal + t*vertical - origin - offset,
-                random_double(time0, time1)
+                lower_left_corner + s*horizontal + t*vertical - origin - offset
             );
         }
 
@@ -65,7 +60,6 @@ class camera {
         vec3 vertical;
         vec3 u, v, w;
         double lens_radius;
-        double time0, time1;  // shutter open/close times
 };
 
 #endif
--- src/raytracer/Raytracer/ray.h
+++ src/raytracer/Raytracer/ray.h
@@ -18,16 +18,15 @@ class ray {
     public:
         ray() {}
         ray(const point3& origin, const vec3& direction)
-            : orig(origin), dir(direction), tm(0)
+            : orig(origin), dir(direction)
         {}
 
         ray(const point3& origin, const vec3& direction, double time)
-            : orig(origin), dir(direction), tm(time)
+            : orig(origin), dir(direction)
         {}
 
         point3 origin() const  { return orig; }
         vec3 direction() const { return dir; }
-        double time() const    { return tm; }
 
         point3 at(double t) const {
             return orig + t*dir;
@@ -36,7 +35,6 @@ class ray {
     public:
         point3 orig;
         vec3 dir;
-        double tm;
 };
 
 #endif
